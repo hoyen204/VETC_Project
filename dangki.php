a516affaf251cfa79a71ef58fe1ec182ce1e5d6f
hoyen204 <48522970+hoyen204@users.noreply.github.com>
2020-10-30 14:14:52 +0700

    30/10 2:14PM

--- dangki.php
+++ dangki.php
@@ -1,69 +1,104 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>Đăng Kí Giảng Viên</title>
 </head>
+
 <body>
 
-    <div style="login-box box-custom" >
-        <p style="" class="login-box-msg">Đăng kí</p>
+    <div style="login-box box-custom">
         <form action="#" method="post" id="fdangki">
-            <ul style="text-align:center;list-style-type:none">
-                <div style="padding: 5px 4px 6px 7px;">
-                    <li >
+            <table align="center">
+                <tr>
+                    <th colspan="2">Đăng kí</th>
+                </tr>
+                <tr>
+                    <td>
+                        <label for="user">Mã giáo viên :</label>
+                    </td>
+                    <td>
                         <input id="user" name="user" type="text" placeholder="Mã Giáo Viên" autofocus="">
-                    </li>
-                    <li>
-                        <input id="pass" name="pass" type="password" placeholder="Mật khẩu">
-                    </li>
-                    <li>
-                        <input id="name" name="name" type="text" placeholder="Tên Giáo Viên">
-                    </li>
-                    <li>
-                        <input id="email" name="email" type="text" placeholder="Email">
-                    </li>
-                    <li>
-                        <input id="user" name="user" type="text" placeholder="Mã Giáo Viên">
-                    </li>
-                    <li>
-                        <input id="user" name="user" type="text" placeholder="Mã Giáo Viên">
-                    </li>
-                    <input id="gvien" name="gvien" type="hidden" value="1">
-                </div>
-            </ul>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <label for="user">Mật khẩu :</label>
+                    </td>
+                    <td>
+                        <input id="pass" name="pass" type="password" placeholder="Mật khẩu" autofocus="">
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <label for="user">Mã giáo viên :</label>
+                    </td>
+                    <td>
+                        <input id="user" name="user" type="text" placeholder="Mã Giáo Viên" autofocus="">
+                    </td>
+
+                </tr>
+                <tr>
+                    <td>
+                        <label for="user">Mã giáo viên :</label>
+                    </td>
+                    <td>
+                        <input id="user" name="user" type="text" placeholder="Mã Giáo Viên" autofocus="">
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <label for="user">Mã giáo viên :</label>
+                    </td>
+                    <td>
+                        <input id="user" name="user" type="text" placeholder="Mã Giáo Viên" autofocus="">
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <label for="user">Mã giáo viên :</label>
+                    </td>
+                    <td>
+                        <input id="user" name="user" type="text" placeholder="Mã Giáo Viên" autofocus="">
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="2" align="center">
+                        <input id="submit" name="submit" type="submit" value="Đăng kí">
+                    </td>
+                </tr>
+            </table>
+
+
         </form>
-        <div style="text-align:center;">
-            <button type="submit" form="fdangki" value="Submit">Đăng kí</button>
-        </div>
     </div>
 
     <style>
-    .login-box, .register-box {
+    .login-box,
+    .register-box {
         width: 400px;
         margin: 10px auto;
         padding: 20px;
         background: #080808;
         opacity: 0.9;
     }
-    .login-box-msg, .register-box-msg {
+
+    .login-box-msg,
+    .register-box-msg {
         margin: 0;
-        text-align: center;
         padding: 0 20px 20px 20px;
         text-align: center;
-        font-size: 20px;;
+        font-size: 20px;
+        ;
         font-weight: bold;
     }
-    .box-custom{
+
+    .box-custom {
         border: 1px solid #cccccc;
         padding: 20px;
         color: #666;
     }
-    li {
-        padding: 5px 50px 6px 7px;
-        margin-top: 3px;
-        margin-bottom: 3px;
-        list-style: none;
-    }
     </style>
 </body>
+
 </html>
